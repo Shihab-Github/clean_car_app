619b0e2d02590ecca2da9cd751109a0921036d63
Raza Shihab Mahbub <raza.shihab@bongobd.com>
2022-12-19 12:56:27 +0600

    user registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./Presentation/Login/Login";
 
+const SignUp = lazy(() => import("./Presentation/SignUp/SignUp"));
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<div>loading...</div>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
--- src/Presentation/SignUp/SignUp.tsx
+++ src/Presentation/SignUp/SignUp.tsx
@@ -4,21 +4,12 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import CustomTextField from "../Shared/CustomTextField";
-import useToast from "../../shared/hooks/useToast";
-import Snackbars from "../../shared/UI/Snackbar";
+import Link from "@mui/material/Link";
 import "./style.css";
-import useLogin from "./hooks/useLogin";
-import { AlertColor } from "@mui/material";
 
-export default function Login() {
-  const [
-    { snackbarOpen, setSnackbarOpen, snackbarMessage, snackbarSeverity },
-    setToast,
-  ] = useToast();
+export default function SignUp() {
   const navigate = useNavigate();
-  const [{ login }] = useLogin();
   const {
     register,
     formState: { errors, isValid },
@@ -28,25 +19,16 @@ export default function Login() {
   });
 
   const redirect = () => {
-    navigate("/register");
-  };
-
-  const submit = () => {
-    let data = {
-      email: getValues("email"),
-      password: getValues("password"),
-    };
-    let res = login(data);
-    if (!res) {
-      setToast("Email or Password incorrect", "error");
-    }
+    navigate("/");
   };
 
   return (
     <>
-      <Box className="login-container">
+      <Box className="signup-container">
         <Stack spacing={2}>
-          <Typography variant="h5">Welcome !</Typography>
+          <Typography variant="h5">
+            Welcome ! Please sign up so that we can help you get started
+          </Typography>
           <CustomTextField
             label={"Email *"}
             type="text"
@@ -75,23 +57,15 @@ export default function Login() {
             }}
             fullWidth
           />
-          <Button variant="contained" onClick={submit} disabled={!isValid}>
+          <Button variant="contained" disabled={!isValid}>
             Login
           </Button>
           <Typography variant="subtitle1" gutterBottom component="div">
-            Don't have an account ? Sign up{" "}
-            <Link underline="always" onClick={redirect}>
-              Here
-            </Link>
+            Already have an account ? Log in{" "}
+            <Link onClick={redirect}>Here</Link>
           </Typography>
         </Stack>
       </Box>
-      <Snackbars
-        open={snackbarOpen}
-        setOpen={setSnackbarOpen}
-        message={snackbarMessage}
-        severity={snackbarSeverity as AlertColor}
-      />
     </>
   );
 }
